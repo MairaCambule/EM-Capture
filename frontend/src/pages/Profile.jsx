491ae528c1d662e387a2dd7a2fbf9bbe97e93c49
MairaCambule <93153378+MairaCambule@users.noreply.github.com>
2026-04-29 16:51:03 +0100

    fix profile supabase

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+//import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../supabaseClient";
 import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
