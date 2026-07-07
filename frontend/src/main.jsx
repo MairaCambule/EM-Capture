4c39bdf7eaee7ee42e23c32843156e01f6eb63f7
MairaCambule <93153378+MairaCambule@users.noreply.github.com>
2026-07-07 16:02:42 +0100

    fix supabase client import 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+
+
+/* import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
@@ -8,3 +25,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+*/
